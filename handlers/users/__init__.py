cfda8d4ae5502ef58fd0bd271bb76f0cb04000e8
Serhii_Tomchuk <dice1@ukr.net>
2021-01-13 00:17:36 +0200

    ContentTypes

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,18 +1,20 @@
 from .admin import dp
-from .help import dp
-from .promotion import dp
 from .poster import dp
+
+
 from .formatting import dp
+from .help import dp
 from .menu import dp
+
+from .promotion import dp
 from .purchase import dp
 from .start import dp
 from .study_task import dp
 from .testing import dp
 from .user_data import dp
-
+from .content_type import dp
 
 from .echo import dp
-
 
 
 __all__ = ["dp"]
